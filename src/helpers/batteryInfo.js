83f1ebee16e12a510145b42349e7e7d6e6824b2c
Howard <howard_bb@hotmail.com>
2022-11-19 23:49:42 +0800

    feat: helper function to get battery info

---
+++ src/helpers/batteryInfo.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { BATTERY_LEVEL_STATUS } = require("../constants/variables");
+
+exports.getBatteryLevel = function () {
+  return new Promise(function (resolve, reject) {
+    const process = require("process");
+    const currentDirectory = process.cwd();
+    const spawn = require("child_process").spawn;
+
+    const pythonProcess = spawn("py", [
+      `${currentDirectory}/../python/getBatteryLevel.py`,
+    ]);
+
+    pythonProcess.stdout.on("data", function (data) {
+      resolve(data);
+    });
+
+    pythonProcess.stderr.on("data", (data) => {
+      reject({ message: data.toString() });
+    });
+  });
+};
+
+exports.getBatteryLevelStatus = function (batterPercentage, isPowerPlugged) {
+  // TODO: get percentage from env or config
+  const isBatteryLow = batterPercentage < 25;
+  const isBatteryExtremeLow = batterPercentage < 10;
+  const isBatteryHigh = batterPercentage > 90;
+
+  if (isBatteryExtremeLow && !isPowerPlugged) {
+    return BATTERY_LEVEL_STATUS.EXTREME_LOW;
+  }
+
+  if (isBatteryLow && !isPowerPlugged) {
+    return BATTERY_LEVEL_STATUS.TO_CHARGE;
+  }
+  if (isBatteryHigh && isPowerPlugged) {
+    return BATTERY_LEVEL_STATUS.STOP_CHARGE;
+  }
+  return BATTERY_LEVEL_STATUS.NORMAL;
+};
