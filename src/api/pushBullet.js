b235f0299d1f60417a2755f4917562a4531448b6
Howard <howard_bb@hotmail.com>
2022-11-19 23:49:21 +0800

    feat: pushbullet push note

---
+++ src/api/pushBullet.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+exports.pushNote = async function ({ title, body }) {
+  try {
+    const response = await axios.post(
+      "https://api.pushbullet.com/v2/pushes",
+      {
+        type: "note",
+        title: title,
+        body: body,
+      },
+      {
+        headers: {
+          // TODO: move key to config.json, or env
+          Authorization: `Bearer o.wiv4UNCCMU6KJxExKhMHB9yAxEMzTZcn`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
