7e6aa3f1cb338090ad48f0f63b4f3203db742a72
Howard <howard_bb@hotmail.com>
2022-11-20 22:58:04 +0800

    feat: add timestamp to notification

--- src/helpers/notification.js
+++ src/helpers/notification.js
@@ -9,22 +9,23 @@ exports.getNotificationDetail = function ({
 }) {
   let title = "";
   let message = "";
+  const timestamp = new Date().toLocaleString();
   switch (batteryLevelStatus) {
     case BATTERY_LEVEL_STATUS.EXTREME_LOW:
       title = `EXTREME LOW! Please charge your device!`;
-      message = `Battery Level: ${batteryPercentage}% \nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
+      message = `Time: ${timestamp}\nBattery Level: ${batteryPercentage}% \nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
       break;
     case BATTERY_LEVEL_STATUS.TO_CHARGE:
       title = `Please charge your device!`;
-      message = `Battery Level: ${batteryPercentage}% \nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
+      message = `Time: ${timestamp}\nBattery Level: ${batteryPercentage}% \nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
       break;
     case BATTERY_LEVEL_STATUS.STOP_CHARGE:
       title = `Stop charging! `;
-      message = `Battery Level: ${batteryPercentage}% \n Is Plugged In: ${isPowerPlugged}`;
+      message = `Time: ${timestamp}\nBattery Level: ${batteryPercentage}% \n Is Plugged In: ${isPowerPlugged}`;
       break;
     default:
       title = `Battery Level Update`;
-      message = `Battery Level: ${batteryPercentage}%\nPlugged in: ${isPowerPlugged}\nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
+      message = `Time: ${timestamp}\nBattery Level: ${batteryPercentage}%\nPlugged in: ${isPowerPlugged}\nRemainingTime: ${remainingTime}\nBattery will die at: ${batteryDieAtString}`;
       break;
   }
   return { title, message };
