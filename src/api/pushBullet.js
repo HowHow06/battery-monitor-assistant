b30f077b18783eafa4d10d05d6c30f855bc4a500
Howard <howard_bb@hotmail.com>
2022-11-20 17:25:10 +0800

    feat: get API key from config json

--- src/api/pushBullet.js
+++ src/api/pushBullet.js
@@ -1,7 +1,12 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/filesync");
+const adapter = new FileSync("./bin/config.json");
 const axios = require("axios");
 
 exports.pushNote = async function ({ title, body }) {
   try {
+    const db = await low(adapter);
+    const pushBulletKey = db.get("pushBulletKey").value();
     const response = await axios.post(
       "https://api.pushbullet.com/v2/pushes",
       {
@@ -12,7 +17,7 @@ exports.pushNote = async function ({ title, body }) {
       {
         headers: {
           // TODO: move key to config.json, or env
-          Authorization: `Bearer o.wiv4UNCCMU6KJxExKhMHB9yAxEMzTZcn`,
+          Authorization: `Bearer ${pushBulletKey}`,
           "Content-Type": "application/json",
         },
       }
