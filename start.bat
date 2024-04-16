57b7ed171b4dad11b514d71dd41350617232f07c
Howard <howard_bb@hotmail.com>
2024-04-16 10:36:53 +0800

    refactor: simplify start batch script

--- start.bat
+++ start.bat
@@ -5,17 +5,7 @@ call venv\Scripts\activate
 if "%~1"=="" (
   yarn start
 ) else (
-  @REM setlocal enabledelayedexpansion and endlocal are used to safely use and modify environment variables within the batch file. 
-  @REM This is useful for handling dynamic values or values that change within a block of code.
-  setlocal enabledelayedexpansion
-
-  
-  @REM "params=%*" captures all arguments as they are, preserving spaces and special characters.
-  set "params=%*"
-  @REM !params! is used to expand the variable at execution time, which ensures that any modifications 
-  @REM (or lack thereof) to the parameters are handled correctly when passing to yarn.
-  yarn !params!
-  endlocal
+  yarn %*
 )
 
 
